adc3033b01854869ac483361596cdd83b25c2e8a
Heldner, Celine <Celine.Heldner@zuehlke.com>
2019-11-05 20:38:42 +0800

    Create data classes that represent the NewsArticle

--- src/main/kotlin/com/zuehlke/NewsAnalyzerKotlin/model/NewsArticle.kt
+++ src/main/kotlin/com/zuehlke/NewsAnalyzerKotlin/model/NewsArticle.kt
@@ -1,5 +1,7 @@
 package com.zuehlke.NewsAnalyzerKotlin.model
 
+import java.time.LocalDateTime
+
 
 /* Created by celineheldner on 9/10/19
 *
@@ -8,3 +10,25 @@ package com.zuehlke.NewsAnalyzerKotlin.model
 *   1. Create a class with the name NewsArticle (getters, equals and copy included - think about data classes in Kotlin)
 *   2. Create the immutable properties author, title, description, url and content.
 * */
+data class NewsArticle (
+        val status: String,
+        val totlaResults: Int,
+        val articles: List<Article>
+)
+
+data class Article (
+        val source: ArticleSource,
+        val author: String,
+        val title: String,
+        val description: String,
+        val url: String,
+        val urlToImate: String,
+        val publishedAt: LocalDateTime,
+        val content: String
+)
+
+data class ArticleSource (
+    val id: String,
+    val name: String
+)
+
