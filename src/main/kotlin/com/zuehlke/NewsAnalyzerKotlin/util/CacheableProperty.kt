7be82db0142851459191f90bac67f5df662be41d
Heldner, Celine <Celine.Heldner@zuehlke.com>
2019-11-19 14:23:35 +0700

    Refine description of exercise

--- src/main/kotlin/com/zuehlke/NewsAnalyzerKotlin/util/CacheableProperty.kt
+++ src/main/kotlin/com/zuehlke/NewsAnalyzerKotlin/util/CacheableProperty.kt
@@ -9,12 +9,15 @@ class CacheableProperty() {
     /*
     TODO Exercise 3
     Create a Property Delegate called Cache. It takes as a constructor argument a duration and a generator-function.
-    The duration specifies how long the property will be cached. f.ex you can create a durationInHour. If the time is over, it will use the generator-function to regenerate the value
+    The duration specifies how long the property will be cached. f.ex you can create a durationInHour.
+    If the time is over, it will use the generator-function to regenerate the value
     The generator-function is only called if the value is not set, or the duration is exceeded.
 
     Implement the property delegation and its getValue operator function, such that it matches the above requirements.
-    Check https://kotlinlang.org/docs/reference/delegated-properties.html for information on how to implement a delegated properties class
+    Check https://kotlinlang.org/docs/reference/delegated-properties.html or https://cehel.github.io/KotlinWorkshopPresentation/slides/03_Delegates.html#/2/1
+    for information on how to implement a delegated properties class
 
+    The goal of this exercise is to be able to implement property delegation. You currently do not need to think of memory issues. (see optional exercise for that)
 
     --Optional or for at home exercise:
     Guava has a nice library for caching, it takes care of many things (also thread safety) related to caching. We can import the library in our build.gradle.kts
