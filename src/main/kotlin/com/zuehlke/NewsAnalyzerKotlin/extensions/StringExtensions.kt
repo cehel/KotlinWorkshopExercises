8cf6ec0341ed150a93593023043071a6526e68a0
Heldner, Celine <Celine.Heldner@zuehlke.com>
2019-11-19 14:44:37 +0700

    Add Hint

--- src/main/kotlin/com/zuehlke/NewsAnalyzerKotlin/extensions/StringExtensions.kt
+++ src/main/kotlin/com/zuehlke/NewsAnalyzerKotlin/extensions/StringExtensions.kt
@@ -6,4 +6,6 @@ package com.zuehlke.NewsAnalyzerKotlin.extensions
 /*
 TODO Exercise 2: implement an extension function on String, called countOccurenceOf(keyword:String),
     which returns the number of times the keyword appeared in that String.
+
+    You can use split("\\s".toRegex()) to get an array of Strings containing each word
  */
