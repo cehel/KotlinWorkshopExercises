343692df9e40e4ceed6d5739a77c447c0c8cbfe6
Heldner, Celine <Celine.Heldner@zuehlke.com>
2019-11-19 08:59:35 +0700

    Remove Todo and description

--- src/main/kotlin/com/zuehlke/NewsAnalyzerKotlin/util/LoggableMutableList.kt
+++ src/main/kotlin/com/zuehlke/NewsAnalyzerKotlin/util/LoggableMutableList.kt
@@ -5,15 +5,20 @@ package com.zuehlke.NewsAnalyzerKotlin.util
     
 /*
 TODO Exercise 4: Create a a LoggableCollection class
-  it should implements all the features of a MutableCollection
-  additionally to that it should log a message each time a result has been added or removed
+  it should implement all the features of a MutableCollection
+  additionally to that it should log a message each time something has been added to the List
 
   Use class delegation to implement such a List
  */
 
-class LoggableMutableList<T>(val collection: MutableCollection<T>): MutableCollection<T> by collection {
+class LoggableCollection<T>(val collection: MutableCollection<T>): MutableCollection<T> by collection {
     override fun add(element: T): Boolean {
         println("Add element $element to list")
         return collection.add(element)
+    }
+
+    override fun addAll(elements: Collection<T>): Boolean {
+        println("Add elements $elements to List")
+        return collection.addAll(elements)
     }
 }
