f7a6c39bd64463dd1eaedb9a6b4a9a932025cd51
Heldner, Celine <Celine.Heldner@zuehlke.com>
2019-11-19 14:32:36 +0700

    Refine description of exercise

--- src/main/kotlin/com/zuehlke/NewsAnalyzerKotlin/model/NewsArticle.kt
+++ src/main/kotlin/com/zuehlke/NewsAnalyzerKotlin/model/NewsArticle.kt
@@ -4,8 +4,10 @@ package com.zuehlke.NewsAnalyzerKotlin.model
 /* Created by celineheldner on 9/10/19
 *
 * TODO: Exercise 1:
-* Create a model which represents the NewsArticle you receive in the JSON
-*   1. Create one or serveral class/es to represent a NewsArticle. Check NewsDataServiceLocal for the model to represent.
+* Create a data model which represents the NewsArticle you receive in the JSON
+*   1. Create serveral classes to represent a NewsArticle data. Check NewsDataServiceLocal for the model to represent.
 *       Hint: getters, equals and copy included - think about data classes in Kotlin
+*
+* look at the data here as well: https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=62fb9d7b1c784acaacf556d98ece1b20
 *
 * */
